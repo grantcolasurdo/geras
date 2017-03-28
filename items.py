a967e0184bba1679495e0956df5c49b087ba8754
Grant Colasurdo <gcolasurdo@ahtins.com>
2017-03-27 17:10:32 -0700

    started database

--- items.py
+++ items.py
@@ -9,37 +9,92 @@ class Items:
 
 
 class Item:
-    def __init__(self, name, weight, size, value):
-        self.item_name: str = name
-        self.weight: float = weight
-        self.size: float = size
-        self.value: float = value
-        self.hands_to_weild: int = None
+    def __init__(self, **kwargs):
+        self.item_name: str = None
+        self.weight: float = None
+        self.size: float = None
+        self.value: float = None
+        self.hands_to_wield: int = None
+        for key, value in kwargs.items():
+            try:
+                assert key in self.__dict__
+                self.__dict__[key] = value
+            except AssertionError:
+                print("There is no key " + key + " in this item")
 
 
 class Currency(Item):
-    def __init__(self, name, weight, size, value):
-        super().__init__(name, weight, size, value)
-        
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+
+class Container(Item):
+    def __init__(self, **kwargs):
+        self._weight = None
+        self.items = set()
+        self.volume_capacity = None
+        self.weight_capacity = None
+        super(Container, self).__init__(**kwargs)
+
+    @property
+    def weight(self) -> float:
+        contained_weight = sum([item.weight for item in self.items])
+        return contained_weight + self.self_weight
+
+    @weight.setter
+    def weight(self, value: float):
+        self._weight = value
+
+    @property
+    def self_weight(self) -> float:
+        return self._weight
+
+    @self_weight.setter
+    def self_weight(self, value: float):
+        self._weight = value
+
+    @property
+    def remaining_weight(self):
+        return self.weight_capacity - self.weight
+
+    @property
+    def occupied_space(self):
+        return sum([item.size for item in self.items])
+
+    @property
+    def remaining_space(self):
+        return self.volume_capacity - self.occupied_space
+
+    def insert(self, item: Item):
+        try:
+            assert self.remaining_space >= item.size
+            self.items.add(item)
+        except AssertionError:
+            print("There is not enough space or spare weight in the container to add")
+
+    def remove(self, item: Item):
+        try:
+            assert item in self.items
+            self.items.remove(item)
+        except AssertionError:
+            print("That item is not in the container")
+
 
 class Weapon(Item):
-    def __init__(
-        self, name, weight, size, value, weapon_group, minimum_strength,
-        min_range, max_range
-    ):
-        super().__init__(name, weight, size, value)
+    def __init__(self, **kwargs):
         self._damage_rolls = None
         self._weapon_group = None
         self._minimum_strength = None
         self._state = None
         self._max_range = None
         self._min_range = None
+        super().__init__(**kwargs)
 
 
-class MissleWeapon(Weapon):
-    def __init__(self):
+class MissileWeapon(Weapon):
+    def __init__(self, ):
         super().__init__()
-        self._missle_used = None
+        self._missile_used = None
 
 
 class Armor(Item):
@@ -60,7 +115,7 @@ class Shield(Item):
 class State:
     def __init__(self):
         self._name = None
-        self._emburdened = None
+        self._burdened = None
 
 
 class Action:
@@ -71,8 +126,6 @@ class Action:
         self._required_state = None
 
 
-
 class MajorAction(Action):
     def __init__(self):
         super().__init__()
-        self._
